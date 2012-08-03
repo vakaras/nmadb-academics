d6edba4f0a05c32ff3ed806a9e2f0ddbec76f0e3
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-08-03 16:58:25 +0300

    Enhanced admin page.

--- src/nmadb_academics/admin.py
+++ src/nmadb_academics/admin.py
@@ -4,9 +4,10 @@ from django.utils.translation import ugettext as _
 from nmadb_academics import models
 from nmadb_students import models as students_models
 from nmadb_students import admin as students_admin
+from nmadb_utils import admin as utils
 
 
-class SectionAdmin(admin.ModelAdmin):
+class SectionAdmin(utils.ModelAdmin):
     """ Administration for section.
     """
 
@@ -35,7 +36,7 @@ class AchievementInline(admin.StackedInline):
     extra = 0
 
 
-class AcademicAdmin(admin.ModelAdmin):
+class AcademicAdmin(utils.ModelAdmin):
     """ Administration for academic.
     """
 
@@ -63,6 +64,57 @@ class AcademicAdmin(admin.ModelAdmin):
             'section__title',
             )
 
+    sheet_mapping = (
+            (_(u'ID'), ('id',)),
+            (_(u'First name'), ('student', 'first_name',)),
+            (_(u'Last name'), ('student', 'last_name',)),
+            (_(u'School'), ('student', 'current_school', 'title')),
+            (_(u'Class'), ('student', 'current_school_class')),
+            (_(u'Section'), ('section', 'title')),
+            (_(u'Entered'), ('entered',)),
+            (_(u'Left'), ('left',)),
+            (_(u'Leaving reason'), ('get_leaving_reason_display',)),
+            )
+
+
+class AchievementAdmin(utils.ModelAdmin):
+    """ Administration for achievement.
+    """
+
+    list_display = (
+            'id',
+            'student',
+            'academic',
+            'competition',
+            'competition_type',
+            'place',
+            )
+
+    list_filter = (
+            'competition_type',
+            'place',
+            )
+
+    search_fields = (
+            'id',
+            'student__first_name',
+            'student__last_name',
+            'student__old_last_name',
+            'competition',
+            )
+
+    sheet_mapping = (
+            (_(u'ID'), ('id',)),
+            (_(u'First name'), ('student', 'first_name',)),
+            (_(u'Last name'), ('student', 'last_name',)),
+            (_(u'School'), ('student', 'current_school', 'title')),
+            (_(u'Class'), ('student', 'current_school_class')),
+            (_(u'Section'), ('academic', 'section', 'title')),
+            (_(u'Competition'), ('competition',)),
+            (_(u'Competition type'), ('get_competition_type_display',)),
+            (_(u'Place'), ('get_place_display',)),
+            )
+
 
 class StudentAdmin(students_admin.StudentAdmin):
     """ Administration for student, who is also an academic.
@@ -78,3 +130,4 @@ admin.site.unregister(students_models.Student)
 admin.site.register(students_models.Student, StudentAdmin)
 admin.site.register(models.Section, SectionAdmin)
 admin.site.register(models.Academic, AcademicAdmin)
+admin.site.register(models.Achievement, AchievementAdmin)
